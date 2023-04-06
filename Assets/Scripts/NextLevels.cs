36fccae92d0f35dc0707d88445b7d1a522054b65
MohamedDev5 <88777716+MohamedDev5@users.noreply.github.com>
2023-04-06 21:43:31 +0200

    ProgramForMap2

---
+++ Assets/Scripts/NextLevels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevels : MonoBehaviour
+{
+    public void ChoseMap2()
+    {
+        SceneManager.LoadScene("MAP2");
+    }
+
+        public void ChoseMap3()
+    {
+        SceneManager.LoadScene("MAP3");
+    }
+}
